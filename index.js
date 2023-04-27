204c26a79a4178eb4c751f440ce6ba04cd72a019
Tejas Gupta <115937671+Tejas-Gupta-7@users.noreply.github.com>
2023-04-28 04:36:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ particlesJS("particles-js", {
       },
       "move": {
         "enable": true,
-        "speed": 0.8,
+        "speed": 1,
         "direction": "none",
         "random": true,
         "straight": false,
